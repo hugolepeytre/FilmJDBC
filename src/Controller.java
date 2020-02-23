5ef36cbc0cfa76ec23d3ccb492d4da3412624ce1
hugolepeytre <hugo.lepeytre@gmail.com>
2020-02-23 17:27:20 +0100

    Started controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,37 @@
 public class Controller extends DBConnect {
     public Controller() {
+        super();
+
+    }
+
+    public void find_all_roles(String actorName) {
+        
+    }
+
+    public void find_all_movies(String actorName) {
+        
+    }
+
+    public void most_movies_per_genre() {
+        
+    }
+
+    public void insert_new_movies() {
+        // To add :
+        // All movie attributes
+        // The category
+        // The publishing company
+        // 
+    }
+
+    public void insert_review(int userId, String text) {
+        
     }
 }
+
+// TODO :
+// 1. Find the name of all roles a given actor performs.
+// 2. Find the name of all movies a given actor appears in.
+// 3. Find which company produces most movies within each genre (comedy, family, scifi, etc)
+// 4. Insert a new movie with director, writers, actors and whatever belongs here.
+// 5. Insert a review of an episode of a series.
